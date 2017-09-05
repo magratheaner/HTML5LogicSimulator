5cdafba6468e4e19c112cc1bb2a5eeae705b9271
Lukas Golombek <magratheaner@gmail.com>
2017-09-05 20:33:35 +0200

    Add placeObject function to place an object on click

--- js/main.js
+++ js/main.js
@@ -1,8 +1,84 @@
 var game = new Phaser.Game('100', '100', Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
+// Class definitions
+// TODO: Split up into separate files later on
+
+/*
+class LogicObject {
+
+    var state;
+    var sprite;
+
+    constructor() {
+        this.state = false;
+        this.sprite = null;
+    }
+
+}
+
+class Gate extends LogicObject {
+
+    var inputConnections;
+
+	constructor() {
+        super();
+        this.inputConnections = [];
+	}
+
+    this.addInputConnection = function(connection) {
+        inputConnections.push(connection);
+    }
+} 
+
+class AndGate extends Gate {
+    
+    constructor(x,y) {
+        super();
+        this.sprite = game.add.Sprite(x, y, 'AND-Gate');
+    }
+
+    // TODO: This will be redefined on creation, define only once using andGate.prototype.andGateFunction
+    this.andGateFunction = function() {
+        for input in this.inputConnections {
+            this.state = this.state && input.state;
+        }
+    }
+}
+
+class Connection extends LogicObject {
+
+    var input;
+    var output;
+
+    constructor(inputObject, outputObject) {
+        super();
+        this.sprite = game.add.Sprite()
+        
+        this.input = inputObject;
+        this.output = outputObject;
+    }
+
+    this.updateState = function() {
+        this.state = inputObject.state;
+    }
+
+}
+
+class Switch extends LogicObject {
+
+    constructor() {
+        super();
+        this.sprite = game.add.Sprite(x, y, 'Switch');
+    }
+
+}
+*/
 function preload() {
 
     game.load.image('AND-Gate', 'assets/AND-Gate-128.png');
+    // TODO: Add the sprites
+    game.load.image('Switch', 'assets/Switch.png')
+    game.load.image('LED', 'assets/LED.png')
 
 }
 
@@ -27,7 +103,6 @@ function create() {
     if (debugFlag) debug = game.add.text(15, 15, 'test1', { fill: '#ffffff' });
     
     sprite.inputEnabled = true;
-    sprite.events.onInputDown.add(listener, this);
     
     andGateSelected = true;
     
@@ -36,12 +111,13 @@ function create() {
     game.input.activePointer.position.copyTo(oldPosition);
     game.input.activePointer.position.copyTo(newPosition);
 
+    sprite.events.onInputUp.add(placeObject, this);
+
 }
 
-function listener() {
-    
-    if (debugFlag) debug.text = 'Click registered!';
-    
+function placeObject() {
+    if (andGateSelected) andGateSelected = false;
+    else andGateSelected = true;
 }
 
 function update() {
@@ -55,6 +131,8 @@ function update() {
             sprite.position.y = newPosition.y - sprite.height / 2;
             if (debugFlag) debug.text = 'New position: ' + newPosition.x + ', ' + newPosition.y;
         }
+
+        if (game.input.activePointer.leftButton.justReleased)
 
         newPosition.copyTo(oldPosition);
     }
